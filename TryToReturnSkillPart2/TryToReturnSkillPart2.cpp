d81261ff9369c1a72a8ed2a4e67a64f7bfa41195
CnUpT <ohpavel92@yandex.ru>
2021-09-09 14:47:10 +0300

    make sme clear

--- TryToReturnSkillPart2/TryToReturnSkillPart2.cpp
+++ TryToReturnSkillPart2/TryToReturnSkillPart2.cpp
@@ -1,7 +1,3 @@
-// TryToReturnSkillPart2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -94,7 +90,7 @@ void write_numbers()
     }
 }
 
-void check_repeat_file() {
+void check_repeat_char_file() {
     std::ifstream fin;
     std::ofstream fon;
     std::vector<std::vector<int>> vecchar;
@@ -144,7 +140,7 @@ void check_repeat_file() {
 int main()
 {
     try {
-        check_repeat_file();
+        check_repeat_char_file();
         keep_window_open();
     }
     //Test throw
@@ -157,13 +153,4 @@ int main()
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
